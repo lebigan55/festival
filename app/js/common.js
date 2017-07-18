0eddd4e827d63abcb3d70666aaa044fbeeffbfdf
lebigan55 <lebigan55@gmail.com>
2017-07-18 17:07:00 +0800

    Поправил медиазапросы для планшетов

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,3 @@
-$(function() {
-
-
-	// Smooth Scroll to block
-	$('a[href^="#l"]').click(function(){
-        var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-        return false; 
-   });
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	};
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-});
-
 $(document).ready(function() {
     $('#fullpage').fullpage({
         navigation: true,
